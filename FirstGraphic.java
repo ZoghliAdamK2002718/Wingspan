ecaf7217a5ead6626703bd43dc7aad7dc5e9f4aa
Daniel Fan <k1416135@students.katyisd.org>
2025-09-30 09:28:46 -0500

    Daniel 9/30 9:28

---
+++ FirstGraphic.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class FirstGraphic extends JFrame{
+private static final int WIDTH = 1200;
+private static final int HEIGHT = 480;
+public FirstGraphic(String framename)
+{
+super(framename);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setSize(WIDTH,HEIGHT);
+add(new WIngSpanGraphic());
+setVisible(true);
+}
+}
---
+++ WIngSpanGraphic.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+public class WIngSpanGraphic extends JPanel
+{
+public WIngSpanGraphic()
+{}
+public void paint(Graphics g)
+{
+super.paint(g);
+g.setColor(Color.BLUE);
+g.drawLine(0, 0, getWidth(), getHeight());
+}
+}
+
+
+
+
