725eabb730feff627d876bff9a4ec954495814f0
Daniel Fan <k1416135@students.katyisd.org>
2025-10-01 08:54:22 -0500

    Daniel 10/1 8:54

--- WIngSpanGraphic.java
+++ WIngSpanGraphic.java
@@ -1,10 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.image.*;
+import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 public class WIngSpanGraphic extends JPanel
 {
 public WIngSpanGraphic()
-{}
+{
+    private BufferedImage c1;
+try {
+    c1=ImageIO.read(new File("AbbottsBooty"));
+} catch (Exception e) {
+}
+}
 public void paint(Graphics g)
 {
 super.paint(g);
