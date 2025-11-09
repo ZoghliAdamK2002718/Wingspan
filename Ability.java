798f6b64fa29ef78b81279b8a17aa53c0a5d1d7c
Anirudh Sreekumar <anirudhsreekumar194@gmail.com>
2025-11-09 06:12:06 -0600

    Anirudh 11/9 6:11

--- Ability.java
+++ Ability.java
@@ -12,14 +12,19 @@ private HashMap<Class,ArrayList<Object>> inputs;
 private final TreeMap<String,ArrayList<String>> keyWords = new TreeMap<String,ArrayList<String>>() {{
     put("play",new ArrayList<String>(Arrays.asList("this", "forest", "wetland", "grassland")));
     put("right",new ArrayList<String>(Arrays.asList()));
-    put("keep",new ArrayList<String>(Arrays.asList()));
+    put("keep",new ArrayList<String>(Arrays.asList()));//use either the bonus card or bird card from the variable set
     put("look",new ArrayList<String>(Arrays.asList()));
-    put("roll",new ArrayList<String>(Arrays.asList("rodent","fish")));//add a return statement for the key word
-    put("draw",new ArrayList<String>(Arrays.asList()));//
-    put("gain",new ArrayList<String>(Arrays.asList()));//
+    put("roll",new ArrayList<String>(Arrays.asList()));//add a return statement for the key word
+    put("draw",new ArrayList<String>(Arrays.asList("equal","NUM","bonus")));//
+    put("gain",new ArrayList<String>(Arrays.asList("NUM","rodent","fish","seed","invertibrate","fruit","wild","supply","available")));// add a return statement for the key word
     put("look",new ArrayList<String>(Arrays.asList("egg","card","hand","seed","fish")));
     put("lay",new ArrayList<String>(Arrays.asList("eggs","another","this","any","each","ground","cavity","burrow","bowl","platform","pendant","cup","scrape","clutch")));
-    put("tuck",new ArrayList<String>(Arrays.asList("NUM")));//
+    put("tuck",new ArrayList<String>(Arrays.asList("NUM","hand","deck","it")));//
+    put("discard",new ArrayList<String>(Arrays.asList("NUM","egg","card","seed","fish","rodent")));
+    put("all",new ArrayList<String>(Arrays.asList()));// does not interupt the flow of other key words if the boolean is true
+    put("you",new ArrayList<String>(Arrays.asList()));
+    put("cache",new ArrayList<String>(Arrays.asList("NUM","it","rodent","fish","seed","invertibrate","fruit","wild","supply","available")));
+    put("selects",new ArrayList<String>(Arrays.asList()));//runs all, and removes the cards that other players choose
 
 }};
 }
